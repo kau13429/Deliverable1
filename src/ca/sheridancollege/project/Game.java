fb60a34a55d7aba6c980837eb8a77f56e1e1290a
Arshdeep kaur <kau13429@sheridancollege.ca>
2023-08-14 22:46:09 -0400

    Game commit

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -16,10 +16,11 @@ import java.util.ArrayList;
  * @author dancye
  * @author Paul Bonenfant Jan 2020
  */
+
 public abstract class Game {
 
     private final String name;//the title of the game
-    private ArrayList<Player> players;// the players of the game
+    private ArrayList<player> players;// the players of the game
 
     public Game(String name) {
         this.name = name;
