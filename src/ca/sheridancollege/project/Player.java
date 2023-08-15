0f99e8f38fcc513e9788936ce2e691328e2100c1
Prabhnoor <grprabhn@sheridancollege.ca>
2023-08-14 22:30:44 -0400

    Prabhnoor player class commited

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -11,32 +11,54 @@ package ca.sheridancollege.project;
  * @author dancye
  * @author Paul Bonenfant Jan 2020
  * @modified Prabhnoor Grewal
- * date 8 July 2023
+ * @date 14 August 2023
+ * 
  */
 public abstract class Player {
 
-    private String name;
-    private int score;
+    private String name;     // The name of the player
+    private int score;       // The score of the player
 
+    /**
+     * Constructor to create a player with a given name.
+     * The player's score is initially set to zero.
+     * 
+     * @param name The name of the player.
+     */
     public Player(String name) {
         this.name = name;
         this.score = 0;
     }
 
+    /**
+     * Get the name of the player.
+     * 
+     * @return The name of the player.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Get the score of the player.
+     * 
+     * @return The score of the player.
+     */
     public int getScore() {
         return score;
     }
 
+    /**
+     * Increment the player's score by 1.
+     */
     public void incrementScore() {
         score++;
     }
 
+    /**
+     * Reset the player's score to zero.
+     */
     public void resetScore() {
-        score+=0;
+        score=0;
     }
 }
-
