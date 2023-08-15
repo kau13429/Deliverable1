f24e331724d8d3dce5096022ad5a8015a043b9d9
Khushpreet Kaur <kau13543@sheridancollege.ca>
2023-08-14 22:01:08 -0400

    Modified Code

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -10,16 +10,61 @@ package ca.sheridancollege.project;
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
  *
  * @author dancye
+ * @modifier Khushpreet Kaur, Student Id: 991685679, Date: 14 August 2023
  */
+
 public abstract class Card {
-    //default modifier for child classes
+    private final String value;  // The value of the card (e.g., "A", "2", "King", "Diamond")
+    private boolean flipped;     // Flag to track if the card is flipped or face-down
 
     /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     * Constructor to initialize the Card with a value.
+     * By default, the card is not flipped (face-down).
+     * 
+     * @param value The value of the card.
+     */
+    public Card(String value) {
+        this.value = value;
+        this.flipped = false;
+    }
+
+    /**
+     * Get the value of the card.
+     * 
+     * @return The value of the card.
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * Check if the card is currently flipped (face-up).
+     * 
+     * @return true if the card is flipped, false otherwise.
+     */
+    public boolean isFlipped() {
+        return flipped;
+    }
+
+    /**
+     * Flip the card to change its state from face-up to face-down or vice versa.
+     */
+    public void flip() {
+        flipped = !flipped;
+    }
+
+    /**
+     * Get a string representation of the card's value.
+     * If the card is flipped, its value is revealed; otherwise, it's shown as "*".
+     * 
+     * @return The string representation of the card.
      */
     @Override
-    public abstract String toString();
-
+    public String toString() {
+        if (flipped) {
+            return value;
+        } else {
+            return "*";
+        }
+    }
 }
